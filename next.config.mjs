9f1373facb1092ff25a02c2f52c69b8782ab0cf4
ArEcZeK362 <bartosz.jaskulski34@gmail.com>
2024-09-17 17:44:16 +0000

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = (phase) => ({
+	reactStrictMode: true,
+
+	env: {
+		SKIP_API_DURING_BUILD: phase === PHASE_PRODUCTION_BUILD ? 'true' : 'false',
+	},
+});
 
 export default nextConfig;
